753b952ff2d8714ccd1ceb5c53ffa4d30655b941
Jerry Tabernero <tabernerojerry@gmail.com>
2023-07-15 18:42:35 +0800

    initial commit

---
+++ src/main/java/me/tabernerojerry/EasyBancoApplication.java
@@ -0,0 +1,13 @@
+package me.tabernerojerry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBancoApplication.class, args);
+	}
+
+}
