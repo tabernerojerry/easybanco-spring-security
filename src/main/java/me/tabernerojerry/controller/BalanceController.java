85e00ff3e090ba003c9e505bae15cd5b79ec64d0
Jerry Tabernero <tabernerojerry@gmail.com>
2023-07-15 19:01:25 +0800

    controllers

---
+++ src/main/java/me/tabernerojerry/controller/BalanceController.java
@@ -0,0 +1,14 @@
+package me.tabernerojerry.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+
+    @GetMapping("/my-balance")
+    public String getBalanceDetails() {
+        return "Balance details from the database";
+    }
+
+}
---
+++ src/main/java/me/tabernerojerry/controller/ContactController.java
@@ -0,0 +1,14 @@
+package me.tabernerojerry.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+    @GetMapping("/contact")
+    public String getContactDetails() {
+        return "Contact details from the database";
+    }
+
+}
