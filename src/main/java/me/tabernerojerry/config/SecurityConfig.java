68e9ffef0f3fe90e320288c545c1dd348fe52a3a
Jerry Tabernero <tabernerojerry@gmail.com>
2023-07-15 19:57:49 +0800

    custom security config

---
+++ src/main/java/me/tabernerojerry/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package me.tabernerojerry.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/contact", "/notices").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
