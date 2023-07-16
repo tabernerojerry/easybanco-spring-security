b7fa4d578ac9e90987643b9385b86fd6f261bcb2
Jerry Tabernero <tabernerojerry@gmail.com>
2023-07-16 22:11:48 +0800

    jdbc user details manager

--- src/main/java/me/tabernerojerry/config/SecurityConfig.java
+++ src/main/java/me/tabernerojerry/config/SecurityConfig.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -24,6 +27,16 @@ public class SecurityConfig {
     }
 
     @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+   /* @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
         UserDetails admin = User.withDefaultPasswordEncoder()
@@ -39,6 +52,6 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(admin, user);
-    }
+    }*/
 
 }
