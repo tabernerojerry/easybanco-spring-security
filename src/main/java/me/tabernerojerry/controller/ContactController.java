f0218c5c59b03f89a7b35273d0b72be1e0871278
Jerry Tabernero <tabernerojerry@gmail.com>
2023-07-19 20:22:19 +0800

    @PreFilter contact name not equal to 'Test'

--- src/main/java/me/tabernerojerry/controller/ContactController.java
+++ src/main/java/me/tabernerojerry/controller/ContactController.java
@@ -3,11 +3,14 @@ package me.tabernerojerry.controller;
 import lombok.RequiredArgsConstructor;
 import me.tabernerojerry.model.Contact;
 import me.tabernerojerry.repository.IContactRepository;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -16,11 +19,17 @@ public class ContactController {
 
     private final IContactRepository contactRepository;
 
+    @PreFilter("filterObject.contactName != 'Test'")
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        Contact contact = contacts.get(0);
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(new Date(System.currentTimeMillis()));
-        return contactRepository.save(contact);
+        contact =  contactRepository.save(contact);
+
+        List<Contact> returnContacts = new ArrayList<>();
+        returnContacts.add(contact);
+        return returnContacts;
     }
 
     public String getServiceReqNumber() {
