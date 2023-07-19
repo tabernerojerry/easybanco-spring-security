2902f2e9ff4ebb6c71b05fe8ae0190249a4a6f39
Jerry Tabernero <tabernerojerry@gmail.com>
2023-07-19 17:36:54 +0800

    removed

--- src/main/java/me/tabernerojerry/EasyBancoApplication.java
+++ src/main/java/me/tabernerojerry/EasyBancoApplication.java
@@ -2,14 +2,12 @@ package me.tabernerojerry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity()
 public class EasyBancoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EasyBancoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBancoApplication.class, args);
+    }
 
 }
