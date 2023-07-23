cb4511effcd06f46e57bded726101116c4719fa4
Jerry Tabernero <tabernerojerry@gmail.com>
2023-07-23 16:51:53 +0800

    pass access token to resource server response with insomnia

--- src/main/java/me/tabernerojerry/controller/LoginController.java
+++ src/main/java/me/tabernerojerry/controller/LoginController.java
@@ -3,16 +3,10 @@ package me.tabernerojerry.controller;
 import lombok.RequiredArgsConstructor;
 import me.tabernerojerry.model.Customer;
 import me.tabernerojerry.repository.ICustomerRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Date;
 import java.util.List;
 
 @RestController
@@ -20,30 +14,6 @@ import java.util.List;
 public class LoginController {
 
     private final ICustomerRepository customerRepository;
-
-    private final PasswordEncoder passwordEncoder;
-
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
-        Customer savedCustomer = null;
-        ResponseEntity<String> response = null;
-        try {
-            String hashPwd = passwordEncoder.encode(customer.getPwd());
-            customer.setPwd(hashPwd);
-            customer.setCreateDt(new Date(System.currentTimeMillis()));
-            savedCustomer = customerRepository.save(customer);
-            if (savedCustomer.getId() > 0) {
-                response = ResponseEntity
-                        .status(HttpStatus.CREATED)
-                        .body("Given user details are successfully registered");
-            }
-        } catch (Exception ex) {
-            response = ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("An exception occured due to " + ex.getMessage());
-        }
-        return response;
-    }
 
     @RequestMapping("/user")
     public Customer getUserDetailsAfterLogin(Authentication authentication) {
